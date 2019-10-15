0dd7bc457641e087e9e0e711e497442adbe2075e
bernardogarza <bernardo.g.landa@gmail.com>
2019-10-15 17:37:57 -0500

    Modified the folders for production to have better organization

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import './style/main.scss';
 import './static/logo-tacos.png';
+
+document.body.innerHTML = `<img src="static/logo-tacos.png" alt="Tacos El Güero">`
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'js/main.js',
     path: path.resolve(__dirname, 'dist'),
   },
 
@@ -48,7 +48,7 @@ module.exports = {
       }
     }),
     new MiniCssExtractPlugin({
-      filename: 'main.css'
+      filename: 'css/[name]-styles.css'
     })
   ]
 };
