2be274aaa3a1d9091d37860e30f9a7e4d00b6ae2
bernardogarza <bernardo.g.landa@gmail.com>
2019-10-21 12:54:53 -0500

    Created the about file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import { pageLoad } from './partials/page-load';
-
-import './style/main.scss';
+import { pageLoad } from './partials/page-load'
+import { about } from './partials/about'
+import './style/main.scss'
 import './static/logo-tacos.png';
 
 const content = document.getElementById('content');
 content.appendChild(pageLoad());
+
+const mainDiv = document.querySelector('.main');
+mainDiv.appendChild(aboutTab());
--- src/partials/page-load.js
+++ src/partials/page-load.js
@@ -1,52 +1,52 @@
 function pageLoad (){
 
-    const containerDiv = document.createElement('div');
-    containerDiv.classList.add('container');
+	const containerDiv = document.createElement('div');
+  containerDiv.classList.add('container');
 
-    const headerDiv = document.createElement('div');
-    headerDiv.classList.add('header');
+	const headerDiv = document.createElement('div');
+	headerDiv.classList.add('header');
 
-    const phoneDiv = document.createElement('div');
-    phoneDiv.innerHTML = `<h4 class="headerInfo">PHONE:<br>83765495</h4>`;
+	const phoneDiv = document.createElement('div');
+	phoneDiv.innerHTML = `<h4 class="headerInfo">PHONE:<br>83765495</h4>`;
 
-    const image = document.createElement('div');
-    image.innerHTML = `<img src="./static/logo-tacos.png" class="logo">`;
-
-
-    const scheduleDiv = document.createElement('div');
-    scheduleDiv.innerHTML = `<h4 class="headerInfo">OPEN EVERYDAY<br>8AM - 3PM<br>6PM - 4AM</h3>`;
+	const image = document.createElement('div');
+	image.innerHTML = `<img src="./static/logo-tacos.png" class="logo">`;
 
 
-    headerDiv.appendChild(phoneDiv);
-    headerDiv.appendChild(image);
-    headerDiv.appendChild(scheduleDiv);
+	const scheduleDiv = document.createElement('div');
+	scheduleDiv.innerHTML = `<h4 class="headerInfo">OPEN EVERYDAY<br>8AM - 3PM<br>6PM - 4AM</h3>`;
 
-    const nav = document.createElement('nav');
-    const ul = document.createElement('ul');
 
-    const menu = document.createElement('li');
-    menu.innerText = 'Menu';
+	headerDiv.appendChild(phoneDiv);
+	headerDiv.appendChild(image);
+	headerDiv.appendChild(scheduleDiv);
 
-    const about = document.createElement('li');
-    about.innerText = 'About';
-    
-    const contact = document.createElement('li');
-    contact.innerText = 'Contact';
+	const nav = document.createElement('nav');
+	const ul = document.createElement('ul');
 
-    ul.appendChild(menu);
-    ul.appendChild(about);
-    ul.appendChild(contact);
-    
-    const main = document.createElement('div');
-    main.classList.add('main');
+	const menu = document.createElement('li');
+	menu.innerText = 'Menu';
 
-    nav.appendChild(ul);
+	const about = document.createElement('li');
+	about.innerText = 'About';
+	
+	const contact = document.createElement('li');
+	contact.innerText = 'Contact';
 
-    containerDiv.appendChild(headerDiv);
-    containerDiv.appendChild(nav);
-    
+	ul.appendChild(menu);
+	ul.appendChild(about);
+	ul.appendChild(contact);
+	
+	const main = document.createElement('div');
+	main.classList.add('main');
 
-    return containerDiv;
+	nav.appendChild(ul);
+
+	containerDiv.appendChild(headerDiv);
+	containerDiv.appendChild(nav);
+	
+
+	return containerDiv;
 }
 
 export { pageLoad };
