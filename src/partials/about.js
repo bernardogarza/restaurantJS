bef4fd57c9663d8b31a6301860733ce425d84d53
bernardogarza <bernardo.g.landa@gmail.com>
2019-10-21 15:28:21 -0500

    Finished with the about style and added an image

--- src/partials/about.js
+++ src/partials/about.js
@@ -2,7 +2,8 @@ function aboutTab() {
   const aboutDiv = document.createElement('div');
   aboutDiv.classList.add('about');
   aboutDiv.innerHTML = `<h1>Who we are?</h1>
-  <p>In 1984 we open our doors in the city of Monterrey, N.L., with a small branch with a capacity for 20 people and the menu was just hamburgers and french fries.<br>In 1990, looking for a bigger menu and more choices for our clients, the arrachera tacos arrived, being our clients favorite. We stand out for our food quality in all of our products, being a 100% family restaurant and our good service.</p>`;
+  <p>In 1984 we open our doors in the city of Monterrey, N.L., with a small branch with a capacity for 20 people and the menu was just hamburgers and french fries.<br>In 1990, looking for a bigger menu and more choices for our clients, the arrachera tacos arrived, being our clients favorite. We stand out for our food quality in all of our products, being a 100% family restaurant and our good service.</p>
+  <img src="./static/about.png" class="logo">`;
   return aboutDiv;
 }
 
