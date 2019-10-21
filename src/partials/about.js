5c35138baaa868397f554116ccebeb920a33a74f
bernardogarza <bernardo.g.landa@gmail.com>
2019-10-21 13:27:03 -0500

    Removed comments

--- src/partials/about.js
+++ src/partials/about.js
@@ -7,8 +7,3 @@ function aboutTab() {
 }
 
 export { aboutTab }
-
-
-// En 1984 abre sus puertas en la ciudad de Monterrey, N.L. el restaurante El Buey, con una pequeña sucursal con capacidad para 20 personas y el menú era sólo hamburguesas y papas a la francesa.
-// En 1990, buscando tener más variedad en el menú y más opciones para nuestros clientes, llegaron los tacos de arrachera en sus diferentes presentaciones, siendo del gusto de todos nuestros clientes. En El Buey nos caracterizamos por brindar un excelente servicio y la mejor calidad en todos nuestros productos y ser un lugar relajado 100% familiar.
-// Agradecemos a todos nuestros clientes que por varias generaciones nos han permitido llegar hasta sus hogares y nos ha mantenido como la mejor opción de tacos de arrachera y hamburguesas.
