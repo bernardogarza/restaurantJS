1a6d01baa8d2523517459eb352c968c793cc4f2d
bernardogarza <bernardo.g.landa@gmail.com>
2019-10-21 13:42:08 -0500

    Created the logic for the tab selection

--- src/index.js
+++ src/index.js
@@ -11,3 +11,26 @@ mainDiv.appendChild(aboutTab());
 
 let navElements = document.querySelectorAll('nav ul li');
 navElements = Array.from(navElements);
+
+const nav = document.querySelector('nav');
+
+nav.addEventListener('click', function(e){
+	for (let element of navElements){
+		element.classList.remove('active');
+	}
+
+	mainDiv.innerHTML = '';
+	console.log(e.target.innerText);
+	if (e.target.innerText == 'About'){
+		mainDiv.appendChild(aboutTab());
+	}
+	else if (e.target.innerText == 'Menu'){
+		mainDiv.appendChild(menuTab());
+	}
+
+	else if (e.target.innerText == 'Contact'){
+		mainDiv.appendChild(contactTab());
+	}
+
+	e.target.classList.add('active');
+})
